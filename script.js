38e5aa76b90068ab8629ab14acdad98398f77fd9
amarfal <amar.alfaleh123@gmail.com>
2025-09-01 16:50:11 -0400

    Implement basic structure for Rock, Paper, Scissors game with HTML and JavaScript files

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const n = Math.floor(Math.random() * 3); // 0, 1, or 2
+    if (n === 0) return "rock";
+    if (n === 1) return "paper";
+    return "scissors";
+  }
+
+  function getHumanChoice() {
+    return prompt("Choose Rock, Paper, or Scissors:")
+  }
